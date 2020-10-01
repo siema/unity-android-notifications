9cf1e4e99d12df62f12326def9972d7123b0f2d2
Siema <garaddon@gmail.com>
2020-10-01 14:44:12 +0200

    add functionality to cancel all pending notifications

--- PluginSrc/app/src/main/java/net/agasper/unitynotification/UnityNotificationManager.java
+++ PluginSrc/app/src/main/java/net/agasper/unitynotification/UnityNotificationManager.java
@@ -10,6 +10,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -18,11 +19,15 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.unity3d.player.UnityPlayer;
 
+import org.json.JSONArray;
+
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import androidx.core.app.NotificationCompat;
@@ -240,5 +245,99 @@ public class UnityNotificationManager extends BroadcastReceiver
         Activity currentActivity = UnityPlayer.currentActivity;
         NotificationManager nm = (NotificationManager)currentActivity.getSystemService(Context.NOTIFICATION_SERVICE);
         nm.cancelAll();
+    }
+
+    public static void CancelAllPendingNotifications()
+    {
+        Activity currentActivity = UnityPlayer.currentActivity;
+        Intent intent = new Intent((Context) currentActivity, UnityNotificationManager.class);
+        try
+        {
+            for (Integer integer : getAlarmIds((Context) currentActivity))
+            {
+                int idAlarm = integer;
+                cancelAlarm((Context) currentActivity, intent, idAlarm);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void cancelAlarm(Context context, Intent intent, int notificationId)
+    {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, notificationId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        pendingIntent.cancel();
+        alarmManager.cancel(pendingIntent);
+        removeAlarmId(context, notificationId);
+    }
+
+    public static boolean hasAlarm(Context context, Intent intent, int notificationId)
+    {
+        return (PendingIntent.getBroadcast(context, notificationId, intent, PendingIntent.FLAG_NO_CREATE) != null);
+    }
+
+    private static void saveAlarmId(Context context, int id)
+    {
+        try
+        {
+            ArrayList<Integer> idsAlarms = getAlarmIds(context);
+            if (idsAlarms.contains(id))
+                return;
+            idsAlarms.add(id);
+            saveIdsInPreferences(context, idsAlarms);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static void removeAlarmId(Context context, int id)
+    {
+        try
+        {
+            ArrayList<Integer> idsAlarms = getAlarmIds(context);
+            for (int i = 0; i < idsAlarms.size(); i++)
+            {
+                if ((Integer) idsAlarms.get(i) == id)
+                    idsAlarms.remove(i);
+            }
+            saveIdsInPreferences(context, idsAlarms);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<Integer> getAlarmIds(Context context)
+    {
+        ArrayList<Integer> ids = new ArrayList<>();
+        try
+        {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            JSONArray jsonArray2 = new JSONArray(prefs.getString(context.getPackageName() + ":alarms", "[]"));
+            for (int i = 0; i < jsonArray2.length(); i++)
+                ids.add(jsonArray2.getInt(i));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return ids;
+    }
+
+    private static void saveIdsInPreferences(Context context, ArrayList<Integer> lstIds)
+    {
+        JSONArray jsonArray = new JSONArray();
+        for (Integer idAlarm : lstIds)
+            jsonArray.put(idAlarm);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(context.getPackageName() + ":alarms", jsonArray.toString());
+        editor.apply();
     }
 }
